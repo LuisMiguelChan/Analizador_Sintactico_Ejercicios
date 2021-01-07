29dcd3f7c22406eaab42678412081fafc6ca33d8
Luis <luis.chankauil@itsva.edu.mx>
2021-01-07 16:51:34 -0600

    Inicio

---
+++ Ejemplo.rb
@@ -0,0 +1,15 @@
+print ( " Bienvenido " )
+t = 33
+y = 3.3
+r = t + y
+print ( " Suma " )
+puts ( r )
+r = t - y
+print ( " Resta " )
+puts ( r )
+r = t * y
+print ( " Multiplicacion" )
+puts ( r )
+r = t / y
+print ( " Division " )
+puts ( r )
